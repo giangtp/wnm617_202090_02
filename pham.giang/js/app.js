c0391a0149b72244ca7844459ba6c87f24ef4a20
giangtp <thuygiangp2000@yahoo.com>
2020-10-02 18:31:02 -0700

    Module 4 Assignment Updated

---
+++ pham.giang/js/app.js
@@ -0,0 +1,12 @@
+$(()=>{
+   $(".tabgroup .tab").on("click", function(e){
+      let index = $(this).index();
+
+      $(this).addClass("active")
+         .siblings().removeClass("active")
+
+      $(this).closest(".tabgroup")
+         .find(".content").eq(index).addClass("active")
+         .siblings().removeClass("active")
+   })
+})
