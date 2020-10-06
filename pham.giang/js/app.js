03b5e9e04d6b8d14ef8bb42fc660438e43509515
giangtp <thuygiangp2000@yahoo.com>
2020-10-06 16:16:16 -0700

    module 5

--- pham.giang/js/app.js
+++ pham.giang/js/app.js
@@ -1,4 +1,46 @@
+// Document Ready
+
 $(()=>{
+
+   checkUserId();
+
+   $(document)
+
+
+   /* FORM SUBMISSIONS */
+
+   // event delegation
+   .on("submit","#signin-form",function(e){
+      e.preventDefault();
+      checkSigninForm();
+   })
+
+
+
+   /* ANCHOR CLICKS */
+
+   .on("click",".js-logout",function(e){
+      sessionStorage.removeItem('userId');
+      checkUserId();
+   })
+
+
+   .on("click","[data-activate]",function(){
+      let target = $(this).data('activate');
+      $(target).addClass("active");
+   })
+   .on("click","[data-deactivate]",function(){
+      let target = $(this).data('deactivate');
+      $(target).removeClass("active");
+   })
+   .on("click","[data-toggle]",function(){
+      let target = $(this).data('toggle');
+      $(target).toggleClass("active");
+   })
+
+
+   /* TABGROUPS */
+
    $(".tabgroup .tab").on("click", function(e){
       let index = $(this).index();
 
@@ -9,4 +51,5 @@ $(()=>{
          .find(".content").eq(index).addClass("active")
          .siblings().removeClass("active")
    })
+
 })
---
+++ pham.giang/js/signin.js
@@ -0,0 +1,37 @@
+//Document Ready
+
+const checkSigninForm = () => {
+   let user = $("#signin-username").val();
+   let pass = $("#signin-password").val();
+
+   if(user == 'user' && pass == 'pass') {
+      // logged in
+      console.log('success');
+      sessionStorage.userId = 3;
+      $("#signin-form")[0].reset();
+   } else {
+      // not logged in
+      console.log('failure');
+      sessionStorage.removeItem('userId');
+
+      // Assignment: What happens if users fail logging in
+   }
+
+   checkUserId();
+}
+
+
+const checkUserId = () => {
+   let p = ['#signin-page','#signup-page',''];
+
+
+   if(sessionStorage.userId === undefined) {
+      // not logged in
+      if(!p.some(o=>window.location.hash===o))
+         $.mobile.navigate("#signin-page");
+   } else {
+      // logged in
+      if(p.some(o=>window.location.hash===o))
+         $.mobile.navigate("#recent-page");
+   }
+}
